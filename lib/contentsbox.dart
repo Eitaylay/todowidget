41355b69d30dcd03449b5fd440e676760e26cb35
Eitaylay <eitaylay2011@gmail.com>
2024-02-23 03:02:19 +0630

    Flutter ToDo List homework

--- lib/contentsbox.dart
+++ lib/contentsbox.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:widgets/todocontents.dart';
+import 'package:todowidgets/todocontents.dart';
 import 'donecontents.dart';
 
 class ContentsBox extends StatelessWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:widgets/titleandmenu.dart';
+import 'package:todowidgets/titleandmenu.dart';
 
 import 'contentsbox.dart';
 
